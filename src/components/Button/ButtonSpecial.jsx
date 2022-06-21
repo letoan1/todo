d554a21545418b1c3c128e55bc31bfc9a93795e7
Toản Lê <85981385+letoan1@users.noreply.github.com>
2022-06-21 23:51:26 +0700

    Revert "Todo API"

--- src/components/Button/ButtonSpecial.jsx
+++ src/components/Button/ButtonSpecial.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/fontawesome-free-regular'
 
-export default function ButtonSpecial({ completed, completedTasks, buttonRef }) {
+export default function ButtonSpecial({ completed, completedTasks }) {
 
   return (
-    <button ref={buttonRef} className={completed ? 'btn--completed' : 'btn btn--special'} onClick={() => completedTasks()}>
+    <button className={completed ? 'btn--completed' : 'btn btn--special'} onClick={() => completedTasks()}>
       <FontAwesomeIcon style={{cursor: 'pointer'}} icon={faCheckCircle} />
     </button>
   )
--- src/components/LayoutTodo/index.jsx
+++ src/components/LayoutTodo/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import Header from '../Header/Header'
 import Input from '../Input/Input'
 import Line from '../Line/Line'
@@ -6,22 +6,20 @@ import ButtonAdd from '../Button/ButtonAdd'
 import List from '../List'
 import Panigation from '../Panigation/Panigation'
 import Select from '../Select/Select'
-import { getAllTasks, deleteTaskById, createTask, getTaskById, updateTaskById } from '../../apis/taskApi'
 
 export default function LayoutTodo() {
-	const [task, setTask] = useState('')
-	const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('todoList')) ?? [])
-	const [tasksRemaining, setTasksRemaining] = useState(0)
-	const [currentPage, setCurrentPage] = useState(1)
-	const [status, setStatus] = useState('all')
-	const [filteredTodos, setFilteredTodos] = useState([])
-	const [showGoToTop, setShowGoToTop] = useState(false)
+	const [task, setTask] = React.useState('')
+	const [tasks, setTasks] = React.useState(JSON.parse(localStorage.getItem('todoList')) ?? [])
+	const [tasksRemaining, setTasksRemaining] = React.useState(0)
+	const [currentPage, setCurrentPage] = React.useState(1)
+	const [status, setStatus] = React.useState('all')
+	const [filteredTodos, setFilteredTodos] = React.useState([])
+	const [showGoToTop, setShowGoToTop] = React.useState(false)
 	const LIMIT_TASK_IN_PAGE = 5
 
-	const inputRef = useRef()
-	const buttonRef = useRef()
+	const inputRef = React.useRef()
 
-	useEffect(() => {
+	React.useEffect(() => {
 		console.log('asdasd');
 		setTasksRemaining(tasks.filter(task => task.completed).length)
 		document.title = task
@@ -30,7 +28,7 @@ export default function LayoutTodo() {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [tasks, task, status])
 
-	useEffect(() => {
+	React.useEffect(() => {
 		const handleScroll = () => {
 			if(window.scrollY >= 200) {
 				setShowGoToTop(true)
@@ -44,20 +42,6 @@ export default function LayoutTodo() {
 		window.removeEventListener('scroll', handleScroll)
 		})
 	}, [])
-
-	useEffect(() => {
-		handleGetAllTasks()
-	}, [])
-
-	const handleGetAllTasks = async () => {
-		try {
-			const data = await getAllTasks()
-			data && setTasks(data)
-			console.log(data)
-		} catch (error) {
-			console.log(error)
-		}
-	}
 
 	const handleClickGoToTop = () => {
 		window.scrollTo({
@@ -84,44 +68,31 @@ export default function LayoutTodo() {
 		setStatus(e.target.value)
 	}
 
-	const handleAdd = async () => {
+	const handleAdd = () => {
 		if(!task.trim()) {
 			alert('Please enter a value')
 		}else{
-			const newTask = { id: new Date().getTime(), task, completed: false }
-			setTasks([ newTask , ...tasks])
+			setTasks([{ id: new Date().getTime(), task, completed: false }, ...tasks])
 			setTask("")
 			inputRef.current.focus()
-
-			try {
-				await createTask(newTask)
-				await handleGetAllTasks()
-			} catch (error) {
-				console.log(error);
-			} finally {
-				setTask('')
-			}
-		}
-
-	}
-
-	const completedTasks = async (id) => {
-		try {
-			const taskById = await getTaskById(id)
-			await updateTaskById(id, { ...taskById, completed: true })
-			await handleGetAllTasks()
-		} catch (error) {
-			console.log(error);
 		}
 	}
 
-	const handleDelete = async (id) => {
-		try {
-			await deleteTaskById(id)
-			await handleGetAllTasks()
-		} catch (error) {
-			console.log(error)
+	const completedTasks = (index) => {
+		const newTasks = [...tasks]
+        if(!newTasks[index].completed) {
+			newTasks[index].completed = true
+		}else{
+			newTasks[index].completed = false
 		}
+
+        setTasks(newTasks)
+	}
+
+	const handleDelete = (id) => {
+		console.log('Delete', id)
+		const deleteTask = tasks.filter(task => task.id !== id)
+		setTasks(deleteTask)
 	}
 
 	const getTaskInCurrentPage = () => {
@@ -160,7 +131,6 @@ export default function LayoutTodo() {
 				completedTasks={completedTasks}
 				getTaskInCurrentPage={getTaskInCurrentPage()}
 				filteredTodos={filteredTodos}
-				buttonRef={buttonRef}
 			/>
 			<Line fullWidth />
 			<Panigation
